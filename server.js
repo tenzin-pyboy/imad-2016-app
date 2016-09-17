83ce8d452f2053ca37961f1ff56d18197e6237aa
Tenzin <ten13zin@gmail.com>
2016-09-18 01:24:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,75 +5,74 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var article = {
+var articles = {
     'article-one': {
-        title: 'Article One | Tenzin ',
-        heading: 'Article One',
-        date: '17 sep, 2016',
-        content: `<p> 
-                    This is my content of the Article one. This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.
-                </p>  
-                <p> 
-                    This is my content of the Article one. This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.
-                </p>
-                <p> 
-                    This is my content of the Article one. This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one.This is my content of the Article one;
-                </p>`
+      title: 'Article One | Tenzin',
+      heading: 'Article One',
+      date: 'Sep 5, 2016',
+      content: `
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>`
     },
     'article-two': {
-        title: 'Article One | Tenzin ',
-        heading: 'Article Two',
-        date: '18 sep, 2016',
-        content: `<p> 
-                    This is my content of the Article two. This is my content of the Article two. This is my content of the Article two. This is my content of the Article two. This is my content of the Article two. This is my content of the Article two. This is my content of the Article two. This is my content of the Article two. This is my content of the Article two. This is my content of the Article two.
-                  </p>`
+      title: 'Article Two | Tenzin',
+      heading: 'Article Two',
+      date: 'Sep 10, 2016',
+      content: `
+          <p>
+              This is the content for my second article.
+          </p>`
     },
     'article-three': {
-        title: 'Article Three | Tenzin ',
-        heading: 'Article Three',
-        date: '19 sep, 2016',
-        content: `<p> 
-                    This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three.
-                </p>  
-               <p> 
-                    This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three. This is my content of the Article three.
-                </p>  
-              `
+      title: 'Article Three | Tenzin',
+      heading: 'Article Three',
+      date: 'Sep 15, 2016',
+      content: `
+          <p>
+              This is the content for my third article.
+          </p>`
     }
 };
 
 function createTemplate (data) {
     var title = data.title;
-    var heading = data.heading;
     var date = data.date;
+    var heading = data.heading;
     var content = data.content;
     
     var htmlTemplate = `
     <html>
-        <head>
-            <title>
-               ${title}
-            </title>
-            <meta name="viewport" content="width-device-width, initial-scale=1"/>
-            <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-            <div class="container">
-                <div>
-                    <a href="/">Home</a>
-                </div>
-                <hr/>
-                <h3> 
-                    ${heading}
-                </h3>
-                <div>
-                    ${date}
-                </div>
-                <div>
-                    ${content}
-                </div>
-            </div>
-        </body>    
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                ${content}
+              </div>
+          </div>
+      </body>
     </html>
     `;
     return htmlTemplate;
@@ -83,11 +82,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res){
-    //articleName == article-one
-    //article[articleName] == {} content of the article-one object
-    var articleName = req.params.articleName;
-   res.send(createTemplate(article[articleName]));
+app.get('/:articleName', function (req, res) {
+  // articleName == article-one
+  // articles[articleName] == {} content object for article one
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
