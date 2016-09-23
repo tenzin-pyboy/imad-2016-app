9e4b41f253135c3dcfc81d1c9298ac372c8ff035
Tenzin <ten13zin@gmail.com>
2016-09-23 16:45:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,9 +107,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) {
   // Get the name from the request
-  var name = req.params.name;
+  var name = req.query.name;
 
   names.push(name);
   // JSON used
