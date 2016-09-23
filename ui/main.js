8d02603bcdea79408103963533a31c1c722c2264
Tenzin <ten13zin@gmail.com>
2016-09-23 17:02:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,7 @@ button.onclick = function () {
   // Capture the respond and store it in a variable
   request.onreadystatechange = function () {
     if(request.readyState == XMLHttpRequest.DONE) {
+      
       //Take acction
       if(request.status === 200){
         var counter = request.responseText;
@@ -29,14 +30,31 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
   // Make a request to the server and send the name
+  
+    // Create a request Object
+  var request = new XMLHttpRequest();
 
-  // Capture a list of names and render it as a list.
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for(var i=0; i<names.length; i++) {
-    list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-
-}
+  // Capture the respond and store it in a variable
+  request.onreadystatechange = function () {
+    if(request.readyState == XMLHttpRequest.DONE) {
+      
+      //Take acction
+      if(request.status === 200){
+        
+        // Capture a list of names and render it as a list.
+          var names = request.responseText;
+          names = JSON.parse(names);
+          var list = '';
+          for(var i=0; i<names.length; i++) {
+            list += '<li>' + names[i] + '</li>';
+          }
+          var ul = document.getElementById('namelist');
+          ul.innerHTML = list;
+      }
+    }
+  };
+  7
+  // Make actual request
+  request.open('GET', 'http://tenzin-pyboy.imad.hasura-app.io/submit-name?name= '+ name, true);
+  request.send(null);
+};
