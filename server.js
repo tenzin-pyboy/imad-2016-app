76f9fe36e523ec94bc34e5cefcf3e307cfc2b27f
Tenzin <ten13zin@gmail.com>
2016-09-28 19:33:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,36 +49,40 @@ function createTemplate (data) {
 
     var htmlTemplate = `
     <html>
-      <head>
-          <title>
+        <head>
+            <title>
               ${title}
-          </title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="/ui/style.css" rel="stylesheet" />
-      </head>
-      <body>
-          <div class="container">
-              <div>
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
                   <a href="/">Home</a>
-              </div>
-              <hr/>
-              <h3>
+                </div>
+                <hr/>
+                <h3>
                   ${heading}
-              </h3>
-              <div>
+                </h3>
+                <div>
                   ${date}
-              </div>
-              <div>
+                </div>
+                <div>
                 ${content}
-              </div>
-              <div class="comment">
-                   <textarea name="message" rows="10" cols="100">
+                </div>
+                <div class="comment">
+                    <textarea name="message" rows="10" cols="100">
                         Start writting Articles Here.
-                   </textarea>
-               <input type='submit' value='Submit' />  
-              </div>
-          </div>
-      </body>
+                    </textarea>
+                <input type='submit' value='Submit' />  
+                </div>
+                </div>
+                    <script type="text/javascript" src="/ui/main.js">
+                    </script>
+                </div>
+            </div>
+        </body>
     </html>
     `;
     return htmlTemplate;
