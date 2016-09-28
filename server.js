8cc253c6aee37334256268787e3f4f1d4872e16e
Tenzin <ten13zin@gmail.com>
2016-09-28 22:46:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ var articles = {
 };
 
 function createTemplate (data) {
-   var title = data.title;
+    var title = data.title;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
