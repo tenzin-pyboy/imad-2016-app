a0e91162528f585205a85286f297ea4d46eb3b3f
Tenzin <ten13zin@gmail.com>
2016-10-23 13:58:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,12 +137,23 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
   // articleName == article-one
   // articles[articleName] == {} content object for article one
   var articleName = req.params.articleName;
-  console.log(articleName);
-  res.send(createTemplate(articles[articleName]));
+  
+  pool.query("SELECT * FROM articles WHERE title = " + articleName, function (err, result) {
+      if(err) {
+          res.status(500).send(err.toString());
+      } else {
+          if(result.rows.length === 0) {
+              res.status(404).send('Article not Found');
+          } else {
+              var articleData = result.rows[0];
+              res.send(createTemplate(articles[articleData]));
+          }
+      }
+  });
 });
 
 
