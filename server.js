e4055b0ded7b9506c9b7f3fe821174b809069437
Tenzin <ten13zin@gmail.com>
2016-10-29 17:39:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,42 +15,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-one': {
-      title: 'Article One | Tenzin',
-      heading: 'Article One',
-      date: 'Sep 5, 2016',
-      content: `
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-          </p>
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-          </p>
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-          </p>`
-    },
-    'article-two': {
-      title: 'Article Two | Tenzin',
-      heading: 'Article Two',
-      date: 'Sep 10, 2016',
-      content: `
-          <p>
-              This is the content for my second article.
-          </p>`
-    },
-    'article-three': {
-      title: 'Article Three | Tenzin',
-      heading: 'Article Three',
-      date: 'Sep 15, 2016',
-      content: `
-          <p>
-              This is the content for my third article.
-          </p>`
-    }
-};
-
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
