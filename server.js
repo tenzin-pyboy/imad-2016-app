343effa6995162680f35f6d6debc5bf6edf453c6
Tenzin <ten13zin@gmail.com>
2016-10-29 17:36:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,8 @@ app.get('/articles/:articleName', function (req, res) {
   // articleName == article-one
   // articles[articleName] == {} content object for article one
   
-   pool.query("SELECT * FROM articles WHERE title = '" + req.params.articleName + "'", function (err, result) {
+   // SELECT * FROM articles WHERE title ='\'; DELETE FROM articles WHERE 'a' = \'a'
+   pool.query("SELECT * FROM articles WHERE title = $1", [req.params.articleName], function (err, result) {
       if(err) {
           res.status(500).send(err.toString());
       } else {
