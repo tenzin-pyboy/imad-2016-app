2243b8a8a59799476907874583d458837b89f02f
Tenzin <ten13zin@gmail.com>
2016-11-06 01:37:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-app.user(bodyParser.json());
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title = data.title;
