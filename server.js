30469ca79bb4037c147eaebf23d37d1c4d88ae61
Tenzin <ten13zin@gmail.com>
2016-11-06 01:59:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.post('/create-user', function (req, res) {
    var password = req.body.password;
    var salt = crypto.randomBytes(128);
    var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUE ($1, $2)', [username, dbString], function(err, username) {
+   pool.query('INSERT INTO "user" (username, password) VALUE ($1, $2)', [username, dbString], function(err, result) {
        if(err) {
             res.status(500).send(err.toString());
         } else {
