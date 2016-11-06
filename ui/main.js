f990a00ccf3a265ebe09ee8e6ed1d7f449a6f527
Tenzin <ten13zin@gmail.com>
2016-11-06 12:55:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,8 +49,8 @@ submit.onclick = function () {
   };
   
   //Submit name
-  var username = document.getElementById('username');
-  var password = document.getElementById('password');
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
   console.log(username);
   console.log(password);
   // Make actual request
