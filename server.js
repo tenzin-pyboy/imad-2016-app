43a890473ad5f3f0309560674d0af9d08e3a4dde
Tenzin <ten13zin@gmail.com>
2016-11-06 23:40:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'tenzin-pyboy',
@@ -17,6 +18,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 function createTemplate (data) {
     var title = data.title;
@@ -117,16 +122,29 @@ app.post('/login', function(req, res) {
                 var salt = dbString.split('$')[2]; // extracting the original salt value
                 var hashedPassword = hash(password, salt); // creating a hash based on the password submitted and the original salt value
                 if(dbString === hashedPassword) {
+                    
+                    //set the session
+                    req.session.auth = {userId: result.rows[0].id};
+                    // set cookie with the session id
+                    // Internally, on the server side, it maps the session id ot the object  
+                    // Now the Object looks like {auth: {userId: id}}
+                    
                     res.send('Credentials are correct :)');
                 } else {
                     res.send(403).send('username/password is invalid ! :(');
                 }
              }
-            res.send('User is sucessfully created: ' + username );
         }
     });
 });
 
+app.get('/check-login', function(req, res) {
+    if(req.session && req.session.auth && req.session.auth.userId) {
+        res.send('You are logged in: ' + req.session.auth.userId.toString());
+    } else {
+        res.send('You are not logged in');
+    }
+});
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     //make a select request
