0f9db12ccda93272f9ed751ba9663fa19c6f4990
Tenzin <ten13zin@gmail.com>
2016-11-09 20:45:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
-function createTemplate (data) {
+function createOutTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
@@ -63,12 +63,12 @@ function createTemplate (data) {
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                   <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Profile</a></li>
-                    <li><a href="#about">Login</a></li>
-                    <li><a href="#contact">Register</a></li>
+                    <li class="active"><a href="/index">Profile</a></li>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>
                   </ul>
                   <button id="logoutBtn" class="btn btn-sm btn-default pull-right" type="submit">Log Out</button>
-                </div><!--/.navbar-collapse -->
+                </div><!-- navbar-collapse -->
               </div>
             </nav>
             <div class="container">
@@ -114,6 +114,94 @@ function createTemplate (data) {
     return htmlTemplate;
 }
 
+function createInTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content; 
+
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+            
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" type="text/css" href="./ui/style.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+            
+            <!-- Optional theme -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <nav class="navbar navbar-inverse navbar-fixed-top">
+              <div class="container">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">App Blog</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="/index">Profile</a></li>
+                  </ul>
+                  <button id="logoutBtn" class="btn btn-sm btn-default pull-right" type="submit">Log Out</button>
+                </div><!-- navbar-collapse -->
+              </div>
+            </nav>
+            <div class="container">
+                <div>
+                  <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                  ${heading}
+                </h3>
+                <hr/>
+                <div>
+                    ${date.toDateString()}
+                </div>
+                <div id="article-para">
+                    ${content}
+                </div>
+                <form action="" method="POST">
+                <fieldset>
+                    <legend> Add a Comment</legend>
+                    <label for="handle">E-mail</label>
+                    <input type="text" name="handle" id="handle" vlue="" maxlength="64" />
+                    <br /><br />
+                    <label for="comment">Comment</label>
+                    <textarea rows="6" cols="50" name="comment" id="comment"></textarea>
+                    <br /><br />
+                    <input type="submit" value="Submit"/>
+                </fieldset>
+                </form>
+                <div>
+                    <script type="text/javascript" src="/ui/main.js">
+                    </script>
+                </div>
+                <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+                <!-- Latest compiled and minified JavaScript -->
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
